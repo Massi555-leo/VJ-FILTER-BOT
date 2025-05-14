9f70effe4e70a2c3ba50340fba8dc2f8bbcad541
Massi555-leo <manuleox444@gmail.com>
2025-05-14 20:23:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,7 +106,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkcents.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'e42c00b3360001c37b9fd0113a86c47b80020f45')
 
